aa2ad37ce5fffef4abe6519d4536a14347c798ca
Rick Strahl <rstrahl@west-wind.com>
2016-06-03 15:40:21 -0700

    First MVC

--- Program.cs
+++ Program.cs
@@ -1,4 +1,10 @@
 ﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Routing;
 
 namespace ConsoleApplication
 {
@@ -6,8 +12,39 @@ namespace ConsoleApplication
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World! Time is: " + 
-                              DateTime.Now);
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
+    }
+
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseMvc();
+        }
+    }
+
+
+    public class HelloWorldController
+    {
+        [HttpGet("api/helloworld")]
+        public object HelloWorld()
+        {
+            return new
+            {
+                message = "Hello World",
+                time = DateTime.Now
+            };
         }
     }
 }
