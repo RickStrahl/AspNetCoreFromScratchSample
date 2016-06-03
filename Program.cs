ac016fed640c88d7fdd659a06e48f901cb4efb63
Rick Strahl <rstrahl@west-wind.com>
2016-06-03 14:12:59 -0700

    HelloWorld

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! Time is: " + 
+                              DateTime.Now);
+        }
+    }
+}
